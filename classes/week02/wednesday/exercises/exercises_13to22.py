7980173d8e101a6cd71a6b28b8b00a40b940bc0d
Nina-babson2025 <nkrishna1@babson.edu>
2025-09-10 15:17:08 +0000

    update exercises_13to22.py

--- classes/week02/wednesday/exercises/exercises_13to22.py
+++ classes/week02/wednesday/exercises/exercises_13to22.py
@@ -4,8 +4,14 @@ from classes.week00.second_class.utils import clear_screen
 #13 - Conditional Logic
 Ask the user for a number and print whether it is positive, negative, or zero.
 '''
-# enter code here
-
+txt = "please enter a number: "
+x = int(input(txt))
+if x > 0:
+    print("number is positive")
+elif x < 0:
+    print("number is negative")
+else:
+    print("number is zero")
 
 
 pause=input('pause')
@@ -15,8 +21,12 @@ clear_screen()
 #14 - Even/Odd Check
 Ask the user for a number and print if it is even or odd.
 '''
-# enter code here
-
+txt = "input a number: "
+x = int(input(txt))
+if x % 2 == 0:
+    print("number is even")
+else:
+    print("number is odd")
 
 
 pause=input('pause')
@@ -26,8 +36,23 @@ clear_screen()
 #15 - Boolean Operators
 Ask the user for two numbers and check if both are positive, either is positive, or none is positive.
 '''
-# enter code here
+txt = "input number: "
+x = int(input(txt))
+if x > 0:
+    print("number is positive")
+elif x < 0:
+    print("number is negative")
+else:
+    print("number is neither positive or negative")
 
+txt = "input number: "
+y = int(input(txt))
+if y > 0:
+    print("number is positive")
+elif y < 0:
+    print("number is negative")
+else:
+    print("number is neither positive or negative")
 
 
 pause=input('pause')
@@ -37,9 +62,8 @@ clear_screen()
 #16 - For Loop
 Print all numbers from 1 to 20, skipping multiples of 3.
 '''
-# enter code here
-
-
+for i in range(0,22,3):
+    print(i)
 
 pause=input('pause')
 clear_screen()
@@ -48,9 +72,14 @@ clear_screen()
 #17 - While Loop
 Ask the user to guess a secret number (hardcoded) until they get it right.
 '''
-# enter code here
 
-
+txt = "Guess the secret number! "
+y = 5
+x = int(input(txt))
+while x == y:
+    print("Congrats! You did it!")
+else:
+    print("Oops try again!")
 
 pause=input('pause')
 clear_screen()
@@ -59,8 +88,12 @@ clear_screen()
 #18 - Break / Continue
 Print numbers 1-10 but stop printing when you reach 7 and skip 3.
 '''
-# enter code here
-
+for i in range(0,11):
+    if i == 3:
+        continue
+    if i == 7:
+        break
+    print(i)
 
 
 pause=input('pause')
@@ -70,9 +103,10 @@ clear_screen()
 #19 - Function Definition
 Write a function square(x) that returns the square of a number and test it.
 '''
-# enter code here
-
-
+txt = "enter number: "
+x = int(input(txt))
+y = x**2
+print(y)
 
 pause=input('pause')
 clear_screen()
