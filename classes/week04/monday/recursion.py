edbfe6892aba0a97974fa70fafa4388ab67cadf3
Nina-babson2025 <nkrishna1@babson.edu>
2025-09-24 13:37:50 +0000

    update

--- classes/week04/monday/recursion.py
+++ classes/week04/monday/recursion.py
@@ -3,7 +3,10 @@ def myPrint(n):
     print("hello", n)       
     myPrint(n + 1)
 
-    
-    
+print(myPrint(1))
 
+def recur_sum(n):
+    if n == 0:
+        return 0
+    else: n + recur_sum(n-1)
     
