bc9f3489d806ae34eaa88413cd4b075db6b801f3
Nina-babson2025 <nkrishna1@babson.edu>
2025-09-26 14:23:24 +0000

    Co-authored-by: delete 8 files rslotpole-babson <rslotpole@babson.edu>

---
+++ review.py
@@ -0,0 +1,10 @@
+name ='Sarah'
+age = 21
+next_age = 22.0
+international = False
+
+print(name)
+
+
+
+
---
+++ sandbox/review.py
@@ -0,0 +1,15 @@
+name ='Sarah'
+age = 21
+next_age = 22.0
+international = False
+
+
+myList =[1,2,3]
+myDict{'apples':'red', 'banana': 'yellow'}
+myTuple= (4,5,6)
+mySet = set(1,1,2,2,3,3)
+
+
+students=['Sarah',21,22.0,False]
+print(students[0])
+
