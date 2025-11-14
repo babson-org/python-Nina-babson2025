35912ec752d1adcdfb7797e56ec7aa7c051c2ade
student-name <student@example.com>
2025-11-14 15:21:17 -0500

    Local save before sync (20251114)

--- labs/lab3/YOUR_CODE/initialize_board.py
+++ labs/lab3/YOUR_CODE/initialize_board.py
@@ -1,15 +1,25 @@
 #Initialize Boards#
-#Create 2D list (rows and cols) filled with blanks for both base board and player board
-def initialize_board(rows=5, cols=5, fill=" "):
-    return [[fill for _ in range(cols)] for _ in range(rows)]
+#Create 3D list (rows and cols) filled with blanks for both base board and player board
+initialize_board = [
+    [["+" for _ in range(cols)] for _ in range(rows)],  # Layer 0 = player board
+    [[" " for _ in range(cols)] for _ in range(rows)]   # Layer 1 = hidden (mine) board
+]
+
+rows, cols = 5,5 #hard code 5x5 grid
+board = initialize_board(rows, cols)
 
 
 # Base board: contains mines and numbers
-from labs.lab3.YOUR_CODE.place_random_mines import place_random_mines
+from place_random_mines import place_random_mines
+base_board = board[1]
 
-base_board = initialize_board(rows=5, cols=5)  # Create empty board
-place_random_mines(base_board, num_mines=5)     # Add 5 mines randomly
-print(base_board)
+
+#base_board = initialize_board(rows=5, cols=5)  # Layer 1 board with mines
+#place_random_mines(base_board, num_mines=5)     # Layer 0 board with hidden mines (player board)
+#print(base_board)
 
 # Player board: what the user sees (hidden cells)
-player_board = initialize_board(rows=5, cols=5, fill="+")
+#player_board = initialize_board(rows=5, cols=5, fill="+")
+player_board = board[0]
+
+print(board)
