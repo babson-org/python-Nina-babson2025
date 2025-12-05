07ddccf137e810bebf072bf12b30d251fc179c5a
student-name <student@example.com>
2025-12-05 12:51:41 -0500

    Local save before sync (20251205)

--- labs/lab4/portfolio/str_method.py
+++ labs/lab4/portfolio/str_method.py
@@ -2,6 +2,9 @@
 def portfolio_str(self):
     """TODO: return a readable summary string. self is a Portfolio
     Example (format is flexible):
-        "Bob has 2 positions and $1,234.56"
+        "Bob has 2 positions and $1,234.56" #positions is number of stocks the person owns
     """
-    return f"COMPLETE THIS LINE"
+
+    return f"{self.client}: has {self.shares} positions with a balance of ${self.cash} cash"
+
+portfolio_str("David")
