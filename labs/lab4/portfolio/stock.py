c5bdbd5476ba58ad6691f85aa0be8285c3cd6925
student-name <student@example.com>
2025-12-05 12:51:56 -0500

    Sync from class repo (20251205)

--- labs/lab4/portfolio/stock.py
+++ labs/lab4/portfolio/stock.py
@@ -1,14 +1,14 @@
 
 class Stock:
     def __init__(self, sym, name, shares=0.0, cost=0.0):
-        """TODO:""" #assigns attributes from parameters
-        self.sym = sym
-        self.name = name
-        self.shares = float(shares)
-        self.cost = float(cost)
-
+        """TODO:"""
+        
+        
+       
 
     def __str__(self):
-        """TODO: include symbol, shares, and cost (format flexible).""" #want a string that symbol, shares, and cost displayed
-        return f"{self.sym}: {self.shares} shares, cost ${self.cost}"
-    
+        """TODO: include symbol, shares, and cost (format flexible)."""
+        
+        return f"COMPLETE THIS LINE"
+        
+        
